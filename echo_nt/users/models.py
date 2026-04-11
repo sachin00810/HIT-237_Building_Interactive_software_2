fcbe4777aa40c1f8095158b376ab9c854f4b91a1
Sachin Kharel <kharelsachin08@gmail.com>
2026-04-11 22:54:17 +0930

    Adding admin.py file in users folder

--- echo_nt/users/models.py
+++ echo_nt/users/models.py
@@ -1,6 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-# Create your models here.
+from django.contrib.auth.models import User
 
-class CustomUser(AbstractUser):
-    pass
+class UserProfile(models.Model):
+    # OneToOne: Extends the default user with extra info
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
