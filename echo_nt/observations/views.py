d0e5c69e4d5ef2854e0c676486373ecb3ca7d053
Sachin Kharel <kharelsachin08@gmail.com>
2026-04-15 21:27:15 +0930

    refine observation class-based views

--- echo_nt/observations/views.py
+++ echo_nt/observations/views.py
@@ -1,48 +1,124 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Observation
+from __future__ import annotations
+
+from datetime import timedelta
+
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.exceptions import PermissionDenied
+from django.db.models import Prefetch
+from django.urls import reverse, reverse_lazy
+from django.utils import timezone
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+
+from fauna.models import Habitat
+
 from .forms import ObservationForm
+from .models import Observation
 
-class ObservationListView(ListView):
-    model = Observation
-    template_name = 'observations/observation_list.html'
-    context_object_name = 'observations'
+
+class ObservationQuerysetMixin:
+    queryset = Observation.objects.select_related("user", "species").prefetch_related(
+        Prefetch(
+            "species__habitats",
+            queryset=Habitat.objects.only("id", "name").order_by("name"),
+        )
+    )
 
     def get_queryset(self):
-        # Using QuerySet APIs: select_related for optimization (ForeignKey), prefetch_related for M2M if needed
-        # excluding observations without notes as an example of exclude()
-        # filter() to only show recent ones or any specific logic, here we just show all ordered by date
-        return Observation.objects.select_related('user', 'species').exclude(notes__exact='').order_by('-date_spotted')
+        return self.queryset
 
-class ObservationDetailView(DetailView):
+
+class AuthorRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
+    raise_exception = True
+    permission_denied_message = "You can only modify your own observations."
+
+    def test_func(self):
+        return self.get_object().user_id == self.request.user.id
+
+    def handle_no_permission(self):
+        if self.request.user.is_authenticated:
+            raise PermissionDenied(self.permission_denied_message)
+        return super().handle_no_permission()
+
+
+class ObservationListView(ObservationQuerysetMixin, ListView):
     model = Observation
-    template_name = 'observations/observation_detail.html'
-    context_object_name = 'observation'
-    
+    template_name = "observations/observation_list.html"
+    context_object_name = "observations"
+    paginate_by = 10
+
     def get_queryset(self):
-        # Optimization
-        return Observation.objects.select_related('user', 'species')
+        queryset = super().get_queryset().exclude(notes__exact="")
+        species_id = self.request.GET.get("species")
+        scope = self.request.GET.get("scope")
+        window = self.request.GET.get("window")
+
+        if species_id and species_id.isdigit():
+            queryset = queryset.filter(species_id=int(species_id))
+        if scope == "mine" and self.request.user.is_authenticated:
+            queryset = queryset.filter(user=self.request.user)
+        if window == "recent":
+            queryset = queryset.filter(
+                date_spotted__gte=timezone.now() - timedelta(hours=24)
+            )
+
+        return queryset.order_by("-date_spotted", "species__name")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["active_scope"] = self.request.GET.get("scope", "all")
+        context["active_window"] = self.request.GET.get("window", "all")
+        context["selected_species_id"] = self.request.GET.get("species", "")
+        return context
+
+
+class ObservationDetailView(ObservationQuerysetMixin, DetailView):
+    model = Observation
+    template_name = "observations/observation_detail.html"
+    context_object_name = "observation"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["can_edit"] = (
+            self.request.user.is_authenticated
+            and self.object.user_id == self.request.user.id
+        )
+        return context
+
 
 class ObservationCreateView(LoginRequiredMixin, CreateView):
     model = Observation
     form_class = ObservationForm
-    template_name = 'observations/observation_form.html'
-    success_url = reverse_lazy('observation-list')
+    template_name = "observations/observation_form.html"
 
-    # Handle business logic to auto-assign the currently logged in user
+    def get_initial(self):
+        initial = super().get_initial()
+        species_id = self.request.GET.get("species")
+        if species_id and species_id.isdigit():
+            initial["species"] = int(species_id)
+        return initial
+
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
-class ObservationUpdateView(LoginRequiredMixin, UpdateView):
+    def get_success_url(self):
+        return reverse("observation-detail", kwargs={"pk": self.object.pk})
+
+
+class ObservationUpdateView(
+    AuthorRequiredMixin, ObservationQuerysetMixin, UpdateView
+):
     model = Observation
     form_class = ObservationForm
-    template_name = 'observations/observation_form.html'
-    success_url = reverse_lazy('observation-list')
+    template_name = "observations/observation_form.html"
 
-class ObservationDeleteView(LoginRequiredMixin, DeleteView):
+    def get_success_url(self):
+        return reverse("observation-detail", kwargs={"pk": self.object.pk})
+
+
+class ObservationDeleteView(
+    AuthorRequiredMixin, ObservationQuerysetMixin, DeleteView
+):
     model = Observation
-    template_name = 'observations/observation_confirm_delete.html'
-    success_url = reverse_lazy('observation-list')
+    template_name = "observations/observation_confirm_delete.html"
+    success_url = reverse_lazy("observation-list")
